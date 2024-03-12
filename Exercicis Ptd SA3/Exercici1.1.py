1dbe710f5e5b87eaaa0c4ffcde9d87873b12cc4d
Gerard-camps <gerard.camps7683@iesmacardona.cat>
2024-03-12 09:37:51 +0100

    2 exercicis

---
+++ Exercicis Ptd SA3/Exercici1.1.py
@@ -0,0 +1,4 @@
+nom = (input("Digues el teu nom :"))
+num_ger = int(input("Digues quants germans o germanes tens :"))
+print (nom.lower()== "gerard")
+print (num_ger >= 3)
---
+++ Exercicis Ptd SA3/exercici 2.1.py
@@ -0,0 +1,6 @@
+num_1 = int(input("Digues un nombre qualsevol :"))
+num_2 = int(input("Digues un altre nombre qualsevol :"))
+if num_1 + num_2 > 10:
+    print ("Es major que 10")
+
+
